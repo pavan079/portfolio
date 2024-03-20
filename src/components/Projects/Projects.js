ee30550bd4c57b08c08a1a81effb533ef9a8ab49
pavan079 <pavankumarreddy14510@gmail.com>
2024-03-20 22:34:49 +0530

    commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, } from "react-bootstrap";
+import Particle from "../Particle";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
